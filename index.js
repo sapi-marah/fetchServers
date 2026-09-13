e74a8115480404bf61a7e2958cc71c3af147b993
sapi-marah <Email-7acd024b3df24e40@teamchat.zoom.us>
2026-09-13 08:38:40 -0700

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,20 @@ server.get('/', function(req, res) {
   res.json(data);
 });
 server.post('/newServer', function(req, res) {
-  let id = Math.floor(Math.random() * 1000000);
-  //use a for() {} to find data
+  function createServer() {
+    var id = Math.floor(Math.random() * 1000000);
+    id = id.toString();
+    let srverExists = false;
+    for (let srver of data) {
+      if (srver.id === id) {
+        srverExists = true;
+      }
+    }
+    if (srverExists === true) {
+      createServer();
+      //Server id already exists.
+    } else if (srverExists === false) {
+      return id;
+    }
+  }
 });
