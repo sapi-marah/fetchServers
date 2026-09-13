48b845f6f2c2b194d3211426c1e0e5956e8a9527
sapi-marah <Email-7acd024b3df24e40@teamchat.zoom.us>
2026-09-13 09:10:07 -0700

    Fix key assignment and add data handling in server

--- index.js
+++ index.js
@@ -24,11 +24,26 @@ server.post('/newServer', function(req, res) {
     } else if (srverExists === false) {
       data.push({
         "id" : id,
-        "key" : await argon2.hash(req.body.key)
+        "key" : await argon2.hash(req.body.key),
+        "data" : []
       });
       return id;
     }
   }
   let id = await createServer();
   return res.status(200).send("Your server has been created\nYour Server id is: " + id);
+});
+server.post('/pushData', async function(req, res) {
+  for (let srver of data) {
+    if (srver.id === req.body.id) {
+      if (await argon2.verify(srver.key, req.body.key)) {
+        srver.data.push(req.body.data);
+        return res.status(200).send("Data has been sent");
+      } else {
+        return res.status(403).send("Invaild key");
+      }
+    } else {
+      return res.status(404).send("Server does not exist.");
+    }
+  }
 });
